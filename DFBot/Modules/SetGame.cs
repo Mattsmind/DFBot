32c9d2e7bd8313cc7013e8d5029a7a14d2805522
Mattsmind <tips.dfmedia@gmail.com>
2018-01-11 07:37:47 -0500

    Added a "mode" for setgame to change from "playing, listening, watching and streaming"

--- DFBot/Modules/SetGame.cs
+++ DFBot/Modules/SetGame.cs
@@ -7,11 +7,11 @@ namespace DFBot.Modules
     public class SetGame : ModuleBase<SocketCommandContext>
     {
         [Command("setgame"), RequireOwner]
-        public async Task SetGameAsync([Remainder]string game)
+        public async Task SetGameAsync(int mode = 1, [Remainder]string game = "You Need to set a game")
         {
             var client = Context.Client as DiscordSocketClient;
 
-            await client.SetGameAsync(game);
+            await client.SetGameAsync(game, null, Discord.StreamType.NotStreaming + mode);
         }
     }
 }
