02c2c188ce50db870f7fd553cdec37f8e4e9d6bf
Mattsmind <tips.dfmedia@gmail.com>
2018-01-12 01:20:21 -0500

    Updated the Weather mod framework.

---
+++ DFBot/Modules/Weather.cs
@@ -0,0 +1,42 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DFBot.Modules
+{
+    [DataContract(Name = "main")]
+    internal class WeatherAPIJson
+    {
+        [DataMember(Name = "placeholder1")]
+        public string Placeholder1 { get; set; }
+
+        [DataMember(Name = "placeholder2")]
+        public string Placeholder2 { get; set; }
+
+    }
+
+    [Group("weather")]
+    public class Weather : ModuleBase
+    {
+        [Command, Alias("help")]
+        public async Task DefaultWeatherCommandAsync()
+        {
+            await ReplyAsync("Default");
+        }
+
+        [Command("current")]
+        public async Task GetWeatherCurrentAsync()
+        {
+            await ReplyAsync("Get Current Weather");
+        }
+
+        [Command("forcast")]
+        public async Task GetWeatherForecastAsync()
+        {
+            await ReplyAsync("Get Weather Forecast");
+        }
+    }
+}
