92d4699f3c227d15ed61fea768e5d7617c1c7ac2
Mattsmind <tips.dfmedia@gmail.com>
2018-01-13 00:12:42 -0500

    Added the Uptime command. Incomplete.

---
+++ DFBot/Modules/Uptime.cs
@@ -0,0 +1,23 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DFBot.Modules
+{
+    public class Uptime : ModuleBase<SocketCommandContext>
+    {
+        public async Task UptimeAsync()
+        {
+            
+            DateTime procStartTime = Process.GetCurrentProcess().StartTime;
+            DateTime timeNow = DateTime.Now;
+
+            
+
+            await ReplyAsync(procStartTime + "\n" + timeNow);
+        }
+    }
+}
