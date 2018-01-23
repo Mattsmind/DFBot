eb837f978e600e9ef82da8dac60b044a65b8ff58
Mattsmind <tips.dfmedia@gmail.com>
2018-01-23 01:25:25 -0500

    SetGame now uses more "human" input.

--- DFBot/Modules/SetGame.cs
+++ DFBot/Modules/SetGame.cs
@@ -7,9 +7,26 @@ namespace DFBot.Modules
     public class SetGame : ModuleBase<SocketCommandContext>
     {
         [Command("setgame"), RequireOwner]
-        public async Task SetGameAsync(int mode = 1, [Remainder]string game = "You Need to set a game")
+        public async Task SetGameAsync(string sMode = "playing", [Remainder]string game = "You Need to set a game")
         {
             var client = Context.Client as DiscordSocketClient;
+            int mode = 0;
+
+            switch (sMode.ToLower())
+            {
+                case "playing":
+                    mode = 1;
+                    break;
+                case "listening":
+                    mode = 2;
+                    break;
+                case "watching":
+                    mode = 3;
+                    break;
+                default:
+                    mode = 1;
+                    break;
+            }
 
             await client.SetGameAsync(game, null, Discord.StreamType.NotStreaming + mode);
         }
