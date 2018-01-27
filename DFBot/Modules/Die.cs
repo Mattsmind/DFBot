03c2ac11baf67c9cf921954c350fcc1db706bb88
Mattsmind <tips.dfmedia@gmail.com>
2018-01-27 14:26:50 -0500

    Added Die command.

---
+++ DFBot/Modules/Die.cs
@@ -0,0 +1,19 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DFBot.Modules
+{
+    [Group("die"), RequireOwner]
+    class Die : ModuleBase<SocketCommandContext>
+    {
+        [Command]
+        public async Task DieAsync()
+        {
+            await ReplyAsync("Terminating....");
+            await Context.Client.StopAsync();
+        }
+    }
+}
