adfb91785c70e143333892286dbcedb26eabbd6c
Mattsmind <tips.dfmedia@gmail.com>
2018-02-02 01:23:26 -0500

    Reworking the Weather Module.

--- DFBot/Modules/Weather.cs
+++ DFBot/Modules/Weather.cs
@@ -8,17 +8,11 @@ using Newtonsoft.Json.Linq;
 
 namespace DFBot.Modules
 {
-
     [Group("weather")]
     [Summary("A collection of tools to get weather information from openweathermap.org")]
     public class Weather : ModuleBase<SocketCommandContext>
     {
-        private string baseUrl = "http://api.openweathermap.org/data/2.5/";
-        private string defaultCityQuery = $"{Program.Configuration["weather:city"]}";
-        private string defaultCountryQuery = $"{Program.Configuration["weather:country"]}";
-        private string defaultUnits = $"{Program.Configuration["weather:units"]}";
-
-        private string appId = $"{Program.Configuration["weather:appid"]}";
+        private WeatherDataHandler weatherData = new WeatherDataHandler();
 
         [Command, Alias("help")]
         [Summary("Provides the help information for the weather module.")]
@@ -36,11 +30,12 @@ namespace DFBot.Modules
 
         [Command("forecast")]
         [Summary("Gets a 3 day weather forecast for a given area.")]
-        public async Task GetWeatherForecastAsync()
+        public async Task GetWeatherForecastAsync(string city = null, string country = null)
         {
-            string url = $"{baseUrl}forecast?q={defaultCityQuery},{defaultCountryQuery}&cnt=1&units={defaultUnits}&APPID={appId}";
-           // Console.WriteLine(url);
-            WebRequest req = WebRequest.Create(@url);
+            string reqType = "forecast";
+            string additionalParams = "&cnt=1";
+
+            WebRequest req = WebRequest.Create(@weatherData.URLBuilder(reqType, city, country, additionalParams));
             req.Method = "GET";
 
             HttpWebResponse resp = req.GetResponse() as HttpWebResponse;
@@ -60,6 +55,44 @@ namespace DFBot.Modules
                 Console.WriteLine(string.Format("Status Code: {0}, Status Description: {1}", resp.StatusCode, resp.StatusDescription));
             }
 
+        }
+    }
+
+    public class WeatherDataHandler
+    {
+
+        public string defaultCity = Program.Configuration["weather:city"];
+        public string defaultCountry = Program.Configuration["weather:country"];
+
+        private string units = $"{Program.Configuration["weather:units"]}";
+        private string baseUrl = "http://api.openweathermap.org/data/2.5/";
+        private string appId = $"{Program.Configuration["weather:appid"]}";
+
+        public string URLBuilder(string reuqestType, string city = null, string countryCode = null, string additionalParams = null)
+        {
+            string cityUrlSeg;
+            string countryUrlSeg;
+
+            if (city == null)
+            {
+                cityUrlSeg = defaultCity;
+            }
+            else
+            {
+                cityUrlSeg = city;
+            }
+
+            if (countryCode == null)
+            {
+                countryUrlSeg = defaultCountry;
+            }
+            else
+            {
+                countryUrlSeg = countryCode;
+            }
+
+            string url = $"{baseUrl}{reuqestType}?q={cityUrlSeg},{countryUrlSeg}&units={units}{additionalParams}&APPID={appId}";
+            return url;
         }
     }
 }
