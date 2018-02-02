3414e7aefa25920b712ad632662f8f837eb51087
Mattsmind <tips.dfmedia@gmail.com>
2018-02-02 03:17:09 -0500

    Removed some lines, left in from debugging.

--- DFBot/Modules/Weather.cs
+++ DFBot/Modules/Weather.cs
@@ -53,7 +53,6 @@ namespace DFBot.Modules
         public string GetWeatherData(string reqType, string additionalParams, string city = null, string country = null)
         {
             string url = URLBuilder(reqType, additionalParams, city, country);
-            Console.WriteLine(url);
             WebRequest req = WebRequest.Create(@url);
             req.Method = "GET";
 
