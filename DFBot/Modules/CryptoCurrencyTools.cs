efeb3a0dcd3adc6b645c00f2c24707a11fb1779d
Mattsmind <tips.dfmedia@gmail.com>
2018-02-08 23:55:30 -0500

    Adding CryptoCurrencyTools. A collection of tools for retriving information on crypto.

---
+++ DFBot/Modules/CryptoCurrencyTools.cs
@@ -0,0 +1,31 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DFBot.Modules
+{
+    [Group("cryptotools"), Alias("bitcoin", "ct", "btc")]
+    [Summary("A set of tools used to retrieve information on various cryptocurrencies.")]
+    class CryptoCurrencyTools : ModuleBase<SocketCommandContext>
+    {
+        [Command, Alias("help")]
+        public async Task CryptoHelpAsync()
+        {
+            EmbedBuilder builder = new EmbedBuilder();
+
+            await ReplyAsync("", false, builder.Build());
+        }
+
+        [Command("pricecheck"), Alias("pc", "price")]
+        [Summary("Returns pricing information.")]
+        public async Task PriceCheckAsync(string currencyPair = "btcusd")
+        {
+            EmbedBuilder builder = new EmbedBuilder();
+
+            await ReplyAsync("", false, builder.Build());
+        }
+    }
+}
