e42ce82e4d95b9440a96e36e0678e44cb597ce00
Mattsmind <tips.dfmedia@gmail.com>
2018-02-13 19:02:47 -0500

    Updated the hello command.

--- DFBot/Modules/Hello.cs
+++ DFBot/Modules/Hello.cs
@@ -1,14 +1,22 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
 using System.Threading.Tasks;
 
 namespace DFBot.Modules
 {
     public class Hello : ModuleBase<SocketCommandContext>
     {
-        [Command("hello")]
+        [Command("hello"), Alias("code", "mygit", "myrepo", "source")]
         public async Task HelloAsync()
         {
-            await ReplyAsync($"Hello, {Context.User.Mention}!");
+            EmbedBuilder builder = new EmbedBuilder();
+
+            builder.WithTitle("Hello I'm DF-Bot!")
+                .WithDescription("**Greetings, " + Context.User.Mention + "**\n\n" +
+                "My code can be found at https://github.com/Mattsmind/DFBot")
+                .WithColor(Color.DarkPurple);
+
+            await ReplyAsync("", false, builder.Build());
         }
     }
 }
