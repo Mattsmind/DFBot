49dd3ff8abac3afd3edc6a49e1f55cd3c2f62bb4
Mattsmind <tips.dfmedia@gmail.com>
2018-02-24 01:54:00 -0500

    Added UserAdministartion.cs, with inital framework.

---
+++ DFBot/Modules/UserAdministration.cs
@@ -0,0 +1,48 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DFBot.Modules
+{
+    [Group("useradmin"), Alias("ua","user")]
+    public class UserAdministration : ModuleBase<SocketCommandContext>
+    {
+        [Command, Alias("help")]
+        public async Task UserAdminHelpInfoAsync()
+        {
+            await ReplyAsync("");
+        }
+
+        [Command("kick")]
+        public async Task KickUserAsync(string username)
+        {
+            await ReplyAsync("");
+        }
+
+        [Command("ban")]
+        public async Task BanUserAsync(string username)
+        {
+            await ReplyAsync("");
+        }
+
+        [Command("unban"), Alias("uban", "ub")]
+        public async Task UnbanUserAsync(string username)
+        {
+            await ReplyAsync("");
+        }
+
+        [Command("mute")]
+        public async Task MuteUserAsync(string username, int time)
+        {
+            await ReplyAsync("");
+        }
+
+        [Command("unmute")]
+        public async Task UnmuteUserAsync(string username)
+        {
+            await ReplyAsync("");
+        }
+    }
+}
